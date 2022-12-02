fde40cfffff1e15ee59862f574adc13bebadc743
Andrey Belokon <belokon1981andrey@gmail.com>
2022-12-02 22:09:54 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { PictureAPI } from './js/picturePpixabayAPI.js';
 import Notiflix from 'notiflix';
-import pictureCardTpl from './templates/pictureСarousel.hbs';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -72,7 +71,44 @@ function addPictures() {
 
 function appendPicteureMarkup(hits) {
   console.log(hits);
-  refs.gallery.insertAdjacentHTML('beforeend', pictureCardTpl(hits));
+  refs.gallery.insertAdjacentHTML('beforeend', renderPictures(hits));
+}
+
+function renderPictures(hits) {
+  const markup = hits
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+                <a class="gallery-item" href="${largeImageURL}"><img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy"/></a>
+                <div class="info">
+                    <p class="info-item">
+                        <b>Likes</b>
+                        <span class="info-item-api">${likes}</span>
+                    </p>
+                    <p class="info-item">
+                        <b>Views</b>
+                        <span class="info-item-api">${views}</span>
+                    </p>
+                    <p class="info-item">
+                        <b>Comments</b>
+                        <span class="info-item-api">${comments}</span>
+                    </p>
+                    <p class="info-item">
+                        <b>Downloads</b>
+                        <span class="info-item-api">${downloads}</span>
+                    </p>
+                </div>
+              </div>`
+    )
+    .join('');
+  return markup;
 }
 
 function clearGallery() {
